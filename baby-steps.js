53808b79c466b9e35522de3bc0be30770a4163d4
Amani-messaoui <66686363+Amani-messaoui@users.noreply.github.com>
2020-08-24 11:35:34 +0100

    Update baby-steps.js

--- baby-steps.js
+++ baby-steps.js
@@ -2,10 +2,4 @@ for (i = 2, sum = 0; i < process.argv.length; i++) {
     sum += +process.argv[i];
     };
     console.log(sum);
-    //let result = 0
-
-    //for (let i = 2; i < process.argv.length; i++) {
-     // result += Number(process.argv[i])
-    //}
-
-    //console.log(result)
+   
