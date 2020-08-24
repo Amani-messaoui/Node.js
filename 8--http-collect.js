ae8eca314a3ca5dfa9f74af24fe3d0d5a3c6c8c8
Amani-messaoui <66686363+Amani-messaoui@users.noreply.github.com>
2020-08-24 11:42:54 +0100

    Update and rename http-collect.js to 8--http-collect.js

--- 8--http-collect.js
+++ 8--http-collect.js
@@ -11,16 +11,3 @@ http.get(url, function(response) {
 console.log(data)
 }))
 });
-//const http = require('http')
-//const bl = require('bl')
-
-//http.get(process.argv[2], function (response) {
- // response.pipe(bl(function (err, data) {
-   // if (err) {
-     // return console.error(err)
-    //}
-    //data = data.toString()
-    //console.log(data.length)
-    //console.log(data)
-  //}))
-//})
