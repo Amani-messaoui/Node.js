e6793c49b894430009ffd762341887c44d313165
Amani-messaoui <messaouiamani13@gmail.com>
2020-08-24 09:21:18 +0100

    node

---
+++ baby-steps.js
@@ -0,0 +1,11 @@
+for (i = 2, sum = 0; i < process.argv.length; i++) {
+    sum += +process.argv[i];
+    };
+    console.log(sum);
+    //let result = 0
+
+    //for (let i = 2; i < process.argv.length; i++) {
+     // result += Number(process.argv[i])
+    //}
+
+    //console.log(result)
